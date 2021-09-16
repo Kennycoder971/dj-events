da945871c7f1fe3626a78e2b6ab3b7cae8980eeb
Kennycoder971 <kennycoder971@gmail.com>
2021-09-16 18:14:29 +0200

    Creating Layout

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,12 @@
 import {useRouter} from 'next/router'
+import Layout from '../../components/Layout'
 
 export default function EventPage() {
     const router = useRouter()
     return (
-        <div>
+        <Layout>
             <h1>My Event</h1>
             <h3>{router.query.slug}</h3>
-        </div>
+        </Layout>
     )
 }
