eb520f5ca7318104a84166a060464a1940c81530
Kennycoder971 <kennycoder971@gmail.com>
2021-09-16 17:48:13 +0200

    first commit

---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+export default function EventPage() {
+    return (
+        <div>
+            <h1>My Event</h1>
+            <p>This is an app to find the latest DJ and other musical events</p>
+            <p>Version: 1.0.0</p>
+        </div>
+    )
+}
