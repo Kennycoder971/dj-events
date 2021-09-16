4f956e02d1c7666d0f8f7237e09e73aa71a05ada
Kennycoder971 <kennycoder971@gmail.com>
2021-09-16 18:13:14 +0200

    Creating the general layout

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,11 @@
+import {useRouter} from 'next/router'
+
 export default function EventPage() {
+    const router = useRouter()
     return (
         <div>
             <h1>My Event</h1>
-            <p>This is an app to find the latest DJ and other musical events</p>
-            <p>Version: 1.0.0</p>
+            <h3>{router.query.slug}</h3>
         </div>
     )
 }
