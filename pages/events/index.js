57c21123768ecc58f024c1d36e2887accbf255e0
Kennycoder971 <kennycoder971@gmail.com>
2021-09-18 10:46:04 +0200

    Fetching sigle event from api

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,28 @@
-import Layout from '@/components/Layout'
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
 
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>My events</h1>
-        </Layout>
-    )
+export default function Events({ events }) {
+  console.log(events);
+  return (
+    <Layout>
+      <h1> Events</h1>
+      {events.length && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: {
+      events: events.events.slice(0, 3),
+      revalidate: 1,
+    },
+  };
 }
